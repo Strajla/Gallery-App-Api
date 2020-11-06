b6a4172975b7b173c5090bc7831fb6bf36859955
Strahinja <skansi97@gmail.com>
2020-11-06 03:14:00 +0100

    Seeding, routes, user model and jwt config

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,7 @@ use App\Http\Controllers\AuthController;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/refresh-token', [AuthController::class, 'refreshToken']);
+Route::post('/logout', [AuthController::class, 'logout']);
