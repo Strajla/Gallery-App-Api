6232dec70e26d0ed3f6045578e07744e79b1d783
Strahinja <skansi97@gmail.com>
2020-11-06 03:20:15 +0100

    Fixing AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Facades\JWTFactory;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Tymon\JWTAuth\JWTManager as JWT;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request) {
+        if (!$token = auth()->attempt($request->only('email', 'password'))) {
+            return response(null, 401);
+        }
+
+        return response()->json(compact('token'));
+
+
+    }
+
+    public function refreshToken() {
+        return [
+            'token' => auth('api')->refresh()
+        ];
+    }
+
+    public function logout() {
+        echo 'im out baby';
+        auth('api')->logout(true);
+    }
 }
